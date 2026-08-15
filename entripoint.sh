c2a356a658c904285dbac945e44794510156d25a
mr.linqu <mr.linqu@gmail.com>
2026-08-15 19:11:56 +0000

    dockerfile fix

--- entripoint.sh
+++ entripoint.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-params="-M 0 -C -N -o ServerAliveInterval 2 -o ServerAliveCountMax 3 -o StrictHostKeyChecking no"
+params="-M 0 -C -N \
+  -o ServerAliveInterval=2 \
+  -o ServerAliveCountMax=3 \
+  -o StrictHostKeyChecking=no \
+"
 
 if [ -z "${HOST}" ]; then
     echo "host required"
